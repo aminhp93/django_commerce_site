52d628f36b278cdef7b5e96ffa09ced6667b4462
aminhp93 <minhpn.org.ec@gmail.com>
2016-12-24 10:12:52 -0800

    add first view to site

--- ecommerce_sites/settings.py
+++ ecommerce_sites/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# if DEBUG:
+#     MEDIA_URL = '/media'
+    
+
+
--- profiles/models.py
+++ profiles/models.py
@@ -6,8 +6,6 @@ from django.db import models
 class profile(models.Model):
 	name = models.CharField(max_length=120)
 	description = models.TextField(default="Description default field")
-	location = models.CharField(max_length=120, default='my location default', blank=True, null=True)
-	job = models.CharField(max_length=120, null=True)
 
 	def __unicode__(self):
 		return self.name
