42a10abaac108933e0aaa275fb0121cb5ddaa390
aminhp93 <minhpn.org.ec@gmail.com>
2016-12-24 10:21:50 -0800

    adding staticfiles

--- ecommerce_sites/settings.py
+++ ecommerce_sites/settings.py
@@ -121,8 +121,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# if DEBUG:
-#     MEDIA_URL = '/media'
-    
+if DEBUG:
+    MEDIA_URL = '/media/'
+    STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "static_only")
+    MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "media")
+    STATICFILES_DIRS = (
+        os.path.join(os.path.dirname(BASE_DIR), "static", "static"),
+    )
+
 
 
